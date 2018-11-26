f76cf1f3f447a407d68aa5f2d0f8cc4a996a6aa6
Surya <ssbannerjee@gmail.com>
2018-11-26 07:24:56 +0530

    added randomizer

--- s3mediauploader/core/models.py
+++ s3mediauploader/core/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+import os
+import random
+
+
+def randomizer(instance, filename):
+    basefilename, file_extension= os.path.splitext(filename)
+    chars= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+    randomstr= ''.join((random.choice(chars)) for x in range(10))
+    basefilename = basefilename[:10]
+    return '{basename}_{randomstring}{ext}'.format(basename= basefilename, randomstring= randomstr, ext= file_extension)
+
 
 class Document(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    upload = models.FileField()
+    upload = models.FileField(upload_to=randomizer, null=True)
