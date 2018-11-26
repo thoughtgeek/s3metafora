808835b8c6bf45836423c072e9dfff4e5f1dcfa6
Surya <ssbannerjee@gmail.com>
2018-11-26 06:03:35 +0530

    removed venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/devopsguy/xoxzo/s3imgupload/latestcommit/s3mediauploader/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
